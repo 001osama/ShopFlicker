3bbc61a242a2a1337cc0c5b0f7dc2482d6d23a70
001osama <osamanasim909@gmail.com>
2023-09-29 15:51:42 +0500

    interceptor implemented

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,10 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { HttpClientModule, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { AuthInterceptor } from './services/interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes) ]
+  providers: [provideRouter(routes), provideHttpClient(withInterceptors([AuthInterceptor]))]
 };
