12576c962e092e62a427a268a526f9e6214cd16e
001osama <osamanasim909@gmail.com>
2023-10-10 16:44:11 +0500

    loader implemented in products

--- src/app/components/public-layout/products/products.component.ts
+++ src/app/components/public-layout/products/products.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ItemComponent } from "../item/item.component";
 import { PublicLayoutService } from '../public-layout.service';
@@ -7,13 +7,16 @@ import { AddCartItem } from 'src/app/models/addCartItem';
 import { environment } from 'src/environments/environment.development';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+import { LoaderComponent } from "../../../shared/loader/loader.component";
+import { LoaderService } from 'src/app/services/loader.service';
 
 @Component({
     selector: 'app-products',
     standalone: true,
     templateUrl: './products.component.html',
     styleUrls: ['./products.component.scss'],
-    imports: [CommonModule, ItemComponent, ReactiveFormsModule]
+    providers:[LoaderService],
+    imports: [CommonModule, ItemComponent, ReactiveFormsModule, LoaderComponent]
 })
 export class ProductsComponent implements OnInit {
     searchForm:FormGroup = new FormGroup({
@@ -21,26 +24,32 @@ export class ProductsComponent implements OnInit {
     });
     apiUrl:string = environment.apiURL;
     products:any[]=[];
-    constructor(private service: PublicLayoutService,
-        private cartService: CartService ) 
+    
+    constructor(private _service: PublicLayoutService,
+        private _cartService: CartService,
+        private _loader:LoaderService ) 
         {
             this.searchForm.get('search')?.valueChanges
             .pipe(
-                debounceTime(1000),
+                debounceTime(1500),
                 distinctUntilChanged(),
-                switchMap( x => this.service.getProducts(x))
+                switchMap( x => this._service.getProducts(x))
             )
-            .subscribe(x => {this.products = x.result; console.log(x)})
+            .subscribe(x => this.products = x.result)
         }
 
     ngOnInit(): void {
-        this.service.getProducts().subscribe( response => this.products = response.result );
+        this._loader.showLoader();
+        this._service.getProducts().subscribe({
+            next: (response) => this.products = response.result,
+            error:(err)=> console.error(err),
+            complete:()=>this._loader.hideLoader()
+        });
     }
 
     addToCart(cartItem:AddCartItem){
-        this.cartService.addToCart(cartItem)
+        this._cartService.addToCart(cartItem)
         .subscribe();
     }
-
 
 }
