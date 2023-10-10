6c487ccece58cc7847ee29262d0c31ae933f2ba4
001osama <osamanasim909@gmail.com>
2023-10-10 16:42:58 +0500

    correction in unsubscription

--- src/app/components/auth-layout/signup/signup.component.ts
+++ src/app/components/auth-layout/signup/signup.component.ts
@@ -37,6 +37,8 @@ export class SignupComponent implements OnInit,OnDestroy{
   }
   
   ngOnDestroy(): void {
-    this.subscription.unsubscribe();
+    if(this.subscription){
+      this.subscription.unsubscribe();
+    }
   }
 }
