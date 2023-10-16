dfcd8689a3c8da1ad033cfc304d8c78721accd15
001osama <osamanasim909@gmail.com>
2023-10-16 22:13:55 +0500

    separate input component implemented

--- src/app/components/auth-layout/login/login.component.ts
+++ src/app/components/auth-layout/login/login.component.ts
@@ -1,16 +1,17 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, RouterLink } from '@angular/router';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
 import { Subscription } from 'rxjs/internal/Subscription';
+import { InputComponent } from "../../../shared/input/input.component";
 
 @Component({
-  selector: 'app-login',
-  standalone: true,
-  imports: [CommonModule, RouterLink, ReactiveFormsModule],
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+    selector: 'app-login',
+    standalone: true,
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss'],
+    imports: [CommonModule, RouterLink, ReactiveFormsModule, InputComponent]
 })
 export class LoginComponent implements OnInit,OnDestroy{
 
@@ -22,11 +23,15 @@ export class LoginComponent implements OnInit,OnDestroy{
     private authService: AuthService,
     private router: Router) {}
 
+    email = new FormControl('',[Validators.required, Validators.email]);
+    password = new FormControl('',[Validators.required, Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm)]);
+      
+
     
     ngOnInit(): void {
       this.form = this._fb.group({
-        email: ['',[Validators.required]],
-        password: ['',[Validators.required, Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm)]]
+        email: this.email,
+        password: this.password
       });
     }
     
@@ -34,10 +39,12 @@ export class LoginComponent implements OnInit,OnDestroy{
       this.result = this.authService.login(this.form.value).subscribe({
         next: res => {
             localStorage.setItem("Token",res.result.token);
+            this.authService.loggedIn();
           },
         error:(err) => console.error(err),
         complete:() => this.router.navigate(['/products'])
         });
+        // console.log(this.form);
       }
     
 
--- src/app/components/auth-layout/signup/signup.component.ts
+++ src/app/components/auth-layout/signup/signup.component.ts
@@ -2,19 +2,27 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, RouterLink } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Subscription } from 'rxjs/internal/Subscription';
+import { InputComponent } from "../../../shared/input/input.component";
 
 @Component({
-  selector: 'app-signup',
-  standalone: true,
-  imports: [CommonModule, RouterLink, ReactiveFormsModule],
-  templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+    selector: 'app-signup',
+    standalone: true,
+    templateUrl: './signup.component.html',
+    styleUrls: ['./signup.component.scss'],
+    imports: [CommonModule, RouterLink, ReactiveFormsModule, InputComponent]
 })
 export class SignupComponent implements OnInit,OnDestroy{
   form!: FormGroup;
   subscription! : Subscription;
+
+
+  username = new FormControl('',Validators.required);
+  email = new FormControl('',[Validators.required, Validators.email]);
+  password = new FormControl('',[Validators.required, Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm)]);
+  confirmPassword = new FormControl('',Validators.required);
+
   constructor(
     private _fb: FormBuilder, 
     private authService: AuthService,
@@ -23,10 +31,10 @@ export class SignupComponent implements OnInit,OnDestroy{
   
   ngOnInit(): void {
     this.form = this._fb.group({
-      username: ['',Validators.required],
-      email: ['',Validators.required],
-      password: ['',Validators.required],
-      confirmPassword: ['',Validators.required]
+      username: this.username,
+      email: this.email,
+      password: this.password,
+      confirmPassword: this.confirmPassword
     });
   }
   
