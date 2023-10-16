08f257929ebd097166bb4b942636110a6ff9cf1e
001osama <osamanasim909@gmail.com>
2023-10-16 22:08:30 +0500

    logged in behavior subject flag

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 import { Observable } from 'rxjs/internal/Observable';
 import { environment } from 'src/environments/environment.development';
 
@@ -10,13 +11,26 @@ import { environment } from 'src/environments/environment.development';
 
 export class AuthService{
     url:string = "api/UsersAuth"
-    constructor(private _http:HttpClient) {        
+    isLoggedIn$ = new BehaviorSubject<boolean>(false);
+
+    constructor(private _http:HttpClient) { 
+        if(this.IsUserLoggedIn()){
+            this.loggedIn();       
+        }
+    }
+//to check user status with observable for dynamic rendering
+    userStatus$(){
+        return this.isLoggedIn$.asObservable();
     }
 
     login(loginReq: Object):Observable<any>{
         return this._http.post(`${environment.apiURL}/${this.url}/login`,loginReq);
     }
-
+    
+    loggedIn(){
+        this.isLoggedIn$.next(true);
+    }
+//to check user status without observable
     IsUserLoggedIn():boolean{
         if(this.getAccessToken()){
             return true;
@@ -34,5 +48,6 @@ export class AuthService{
 
     logOut(){
         localStorage.clear();
+        this.isLoggedIn$.next(false);
     }
 }
