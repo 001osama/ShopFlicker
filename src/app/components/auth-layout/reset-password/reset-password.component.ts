6686b1f99f5786faadae6ea9bf2d54f96ed90a96
001osama <osamanasim909@gmail.com>
2023-11-07 01:06:51 +0500

    reset password component created

--- src/app/components/auth-layout/reset-password/reset-password.component.ts
+++ src/app/components/auth-layout/reset-password/reset-password.component.ts
@@ -1,14 +1,43 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-reset-password',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, FormsModule],
   templateUrl: './reset-password.component.html',
   styleUrls: ['./reset-password.component.scss']
 })
 export class ResetPasswordComponent {
 
+  constructor(
+    private _authService:AuthService,
+    private _router:Router) {}
+
+    sendingRequest = false;
+
+  form = {
+    email : '',
+  };
+
+  
+
+  sendResetPasswordReq(){
+    this.sendingRequest = true;
+    console.log(this.form.email);
+    this._authService.resetPassword(this.form.email).subscribe(
+      {
+        complete:()=>{
+          this._router.navigate(['/emailSent']);
+          this.sendingRequest = true;
+        },
+        error: (err)=> {
+          console.error(err);
+          this.sendingRequest = true;
+        }
+      });
+  }
 }
