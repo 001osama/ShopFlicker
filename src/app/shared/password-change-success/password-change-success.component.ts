e61910b2f901fe0e73ec67088acfcaaf6aee83de
001osama <osamanasim909@gmail.com>
2023-11-09 14:48:59 +0500

    password changed success and failed completed

---
+++ src/app/shared/password-change-success/password-change-success.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-password-change-success',
+  standalone: true,
+  imports: [CommonModule, RouterLink],
+  templateUrl: './password-change-success.component.html',
+  styleUrls: ['./password-change-success.component.scss']
+})
+export class PasswordChangeSuccessComponent {
+
+}
